81f28a2ff72dca9435878ba12f164e878d0d38f6
steve315 <steveosoro@gmail.com>
2023-03-16 21:08:39 +0000

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program that prints a string using printf
+ *
+ * Return 0: success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
