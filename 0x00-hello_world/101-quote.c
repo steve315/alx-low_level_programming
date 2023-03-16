cff9269f50dd7140e583dcc854cbd798d99a6eae
steve315 <steveosoro@gmail.com>
2023-03-16 22:24:22 +0000

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Program that prints a strint to the standard output
+ * Return: 1
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
