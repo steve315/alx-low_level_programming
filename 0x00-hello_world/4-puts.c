5c19bd3d86774c5a8307581d4e990b8416040a1e
steve315 <steveosoro@gmail.com>
2023-03-16 20:48:08 +0000

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+   char alx_str[] = "Programming is like building a multilingual puzzle";
+
+   puts(alx_str);
+   return (0);
+}
