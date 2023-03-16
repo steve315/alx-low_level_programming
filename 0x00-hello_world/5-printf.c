6c30ebda231d006df2d88e540561e669f8018cae
steve315 <steveosoro@gmail.com>
2023-03-16 21:13:16 +0000

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - Program that prints a string using printf
- *
- * Return 0: success
+ * main - Entry points
+ * Program that prints a string using printf
+ * Return: 0 success
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
+
 return (0);
 }
