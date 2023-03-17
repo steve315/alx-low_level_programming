289f802162a7d5c048199f396a932ebe5a4f39c9
steve315 <steveosoro@gmail.com>
2023-03-17 14:37:53 +0000

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Program that prints single integers from 0 - 10
+ * Return: 0
+ */
+int main(void)
+{
+	/*declaring variables*/
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+return (0);
+}
