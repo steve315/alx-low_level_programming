09a856b098b7a89d4ecd12b7bfa72b8a830fe650
steve315 <steveosoro@gmail.com>
2023-03-21 14:12:06 +0000

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two numbers and returns the result
+ * @x: parameter 1
+ * @y: parameter 2
+ * Return: 0 on success
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <time.h>
+/**
+ * jack_bauer - prints every minute of the day
+ */
+void jack_bauer(void)
+{
+
+}
