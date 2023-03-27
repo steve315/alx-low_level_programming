7ead9d29707ea26282f2075452d881da3ce79b25
steve315 <steveosoro@gmail.com>
2023-03-27 08:01:08 -0700

    Task 0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that takes a piinter and updates to 98
+ * @n : pointer variable
+ * Return: 0 on success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
