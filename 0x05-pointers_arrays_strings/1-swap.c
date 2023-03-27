d0c1e89ae475420c01b24a0cb549dc88f99ddffd
steve315 <steveosoro@gmail.com>
2023-03-27 08:15:59 -0700

    header file update

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int-function that swaps the values of two integers
+ * @a: pointer to variable a
+ * @b: pointer to variable b
+ * return:0
+ */
+void swap_int(int *a, int *b)
+{
+	int i;
+
+	i = *a;
+	*a = *b;
+	*b = i
+}
