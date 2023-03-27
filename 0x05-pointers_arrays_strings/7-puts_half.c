bbe8cc8a00d3cd48f333245e8550da679d99f3a8
steve315 <steveosoro@gmail.com>
2023-03-27 10:56:37 -0700

    Task 7: Winning is only half of it

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: string pointer
+ * Returns: 0 on success
+ */
+void puts_half(char *str)
+{
+	int i;
+	int j;
+	int length = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		length = 0;
+	}
+
+	if ((length % 2) == 1)
+	{
+		j = ((length + 1) / 2);
+	}
+	else
+	{
+		j = (length / 2);
+	}
+	for (i = j; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+_putchar('\n');
+}
