4e57f40df40c4619cee2533d24cc811e9bb8ff4b
steve315 <steveosoro@gmail.com>
2023-03-30 16:30:46 +0000

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,22 +4,22 @@
  * @s: input string
  * Return: 0 on success
  */
-char *leet(char *n)
+char *leet(char *s)
 {
 	int i, j;
 
 	char n1[] = "aAeEoOtTlL";
 	char n2[] = "4433007711";
 
-	for (i = 0; n[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (n[i] == n1[j])
+			if (s[i] == n1[j])
 			{
-				n[i] = n2[j];
+				s[i] = n2[j];
 			}
 		}
 	}
-	return (n);
+	return (s);
 }
