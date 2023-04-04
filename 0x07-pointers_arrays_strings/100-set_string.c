d7b32fa04842694d95374a3719f7b7dab582ae2d
steve315 <steveosoro@gmail.com>
2023-04-04 21:58:00 +0000

    Pointers

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * set_string - sets the value of a pointer to a char
- * @s: double pointer to s
+ * @s: double pointer s
  * @to: char pointer
  * Return: 0 on success
  */
