fe00f38d993954be397bf31d410abcbf1292a9a2
steve315 <steveosoro@gmail.com>
2023-04-13 13:15:30 +0000

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,12 +31,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (nptr == NULL)
 	{
 		free(ptr);
-		free(nptr);
 		return (NULL);
 	}
 	if (new_size > old_size)
 	{
-		for (i = 0; i < old_size; i++)
+		for (i = 0; i < (old_size || new_size); i++)
 		{
 			*(nptr + i) = *(temp + i);
 		}
