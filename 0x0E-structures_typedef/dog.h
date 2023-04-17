3d2c9dd5707ceac1cfb346d4d0ad33a6af7bb99d
steve315 <steveosoro@gmail.com>
2023-04-17 07:54:28 +0000

    Task 0: dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - creates a dog structure
+ * @name: defines the name
+ * @age: defines the age
+ * @owner: owner of the @name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
