e4872b97125fa51e395acd457ef9b8dc72899518
steve315 <steveosoro@gmail.com>
2023-04-26 06:55:46 +0300

    Deletion

--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,4 +0,0 @@
-#ifndef _FUN_MACRO_
-#define _FUN_MACRO_
-#define ABS(x) ((x < (0)) ? ((x) * (-1)) : (x))
-#endif
--- 0x0D-preprocessor/4-sum.h
+++
@@ -1,4 +0,0 @@
-#ifndef _SUM_MACRO_
-#define _SUM_MACRO_
-#define SUM(X, Y) ((X) + (Y))
-#endif
