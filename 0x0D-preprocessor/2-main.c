ddac2f957caee6af1bd8b2d3217d646f4c90dfd3
steve315 <steveosoro@gmail.com>
2023-09-12 21:50:50 +0300

    pre-processor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: program that prints name of cmp file name
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
