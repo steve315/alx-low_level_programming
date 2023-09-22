3d8137c5444911fb582be875b7b651e868235e14
steve315 <steveosoro@gmail.com>
2023-09-22 11:20:48 +0300

    task 5 - excecutes before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * before - function excecuted before main
+ * Return: 0 on success
+ */
+void before(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
