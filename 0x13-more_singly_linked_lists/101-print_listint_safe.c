36992cd6a1297c53f56304e02bcf42e25654d36a
steve315 <steveosoro@gmail.com>
2023-09-26 23:37:56 +0300

    task 12 betty

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,35 +3,35 @@
  * print_listint_safe - prints list in safe mode
  * @head: first element in list
  * Return: count
-*/
+ */
 size_t print_listint_safe(const listint_t *head)
 {
-    const listint_t *node = head;
-    const listint_t *loop = head;
-    const listint_t *node2 = head;
+	const listint_t *node = head;
+	const listint_t *loop = head;
+	const listint_t *node2 = head;
 
-    size_t count = 0;
+	size_t count = 0;
 
-    while (node2 != NULL && node2->next != NULL)
-    {
-        loop = loop->next;
-        node2 = node2->next->next;
-        count++;
+	while (node2 != NULL && node2->next != NULL)
+	{
+		loop = loop->next;
+		node2 = node2->next->next;
+		count++;
 
-        if (loop == node2)
-        {
-            exit(98);
-        }
-    }
+		if (loop == node2)
+		{
+			exit(98);
+		}
+	}
 
-    node = head;
+	node = head;
 
-    while (node != NULL)
-    {
-        printf("[%p] %d\n",(void *)node, node->n);
-        node = node->next;
-        count++;
-    }
+	while (node != NULL)
+	{
+		printf("[%p] %d\n", (void *)node, node->n);
+		node = node->next;
+		count++;
+	}
 
-    return count;
+	return (count);
 }
