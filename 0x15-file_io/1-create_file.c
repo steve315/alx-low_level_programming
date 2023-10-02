5048a56d29865a35b617eba4fbdaede683d1f9bb
steve315 <steveosoro@gmail.com>
2023-10-02 22:28:07 +0300

    task 1 corr

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,28 +7,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	int file_des = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	ssize_t text_len;
-	ssize_t written_b;
+	int written_b = 1;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-    if (file_des == -1)
-    {
-        return (-1);
-    }
-	if (text_content == NULL)
+	if (file_des == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
 	{
 		text_len = _strlen(text_content);
 		written_b = write(file_des, text_content, text_len);
-
-		if (written_b == -1 || written_b != text_len)
-		{
-			close(file_des);
-			return (-1);
-		}
+	}
+	if (written_b == -1)
+	{
+		return (-1);
 	}
 	close(file_des);
 
