b64ea6dbd4ceba29ed888122440c86910f66a54c
steve315 <steveosoro@gmail.com>
2023-10-02 22:02:41 +0300

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @text_content: NULL terminated string to write to the file
+ * Return: success 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	ssize_t text_len;
+	ssize_t written_b;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		text_len = _strlen(text_content);
+		written_b = write(file_des, text_content, text_len);
+
+		if (written_b == -1 || written_b != text_len)
+		{
+			close(file_des);
+			return (-1);
+		}
+	}
+	close(file_des);
+
+	return (1);
+}
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string pointer
+ * Return: 0
+ */
+ssize_t _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
